fb762fab8c3b20c7fc6696ac7bb156f0efb4224c
doge-hue <dogecheems858@gmail.com>
2023-03-19 13:03:59 +0530

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const path = require('path')
+const path = require('path');
+const cors = require('cors');
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -10,12 +11,18 @@ app.use(express.json());
 const connectDB = require("./config/db");
 connectDB();
 
+//cors
+const corsOption = {
+    origin : process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOption));
+
 //Routes
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));  
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
