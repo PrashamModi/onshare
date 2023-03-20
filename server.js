e5bf4f8d3e3d52543386cb531f47e95f160c1655
doge-hue <dogecheems858@gmail.com>
2023-03-20 11:42:14 +0530

    commm

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.static('../frontend'))
 
 const connectDB = require("./config/db");
 connectDB();
